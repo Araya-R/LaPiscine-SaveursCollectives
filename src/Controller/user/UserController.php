0b6d95094f4011c43d9918dd518fe36055a3e0ae
Araya RAKSACHOB <araya.raksachob@lapiscine.pro>
2026-01-28 23:42:52 +0100

    ajouts commentaires

--- src/Controller/user/UserController.php
+++ src/Controller/user/UserController.php
@@ -19,9 +19,14 @@ class UserController extends AbstractController
 
     //SHOW USER PROFILE
     #[Route('/user/profile', name: 'user-show-profile')]
+
+    // affichage du profil utilisateur/accessible uniquement si l'user est connecté
     public function showProfile() {
+        
+        // on récupère l'user connecté
         $user=$this->getUser();
 
+        // bloque l'aacès si l'user n'est pas connecté
         if(!$user){
             throw $this->createAccessDeniedException('Vous devez être connecté pour voir votre profil.');
         }
@@ -40,38 +45,51 @@ class UserController extends AbstractController
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
         
+        // sécurité: accès uniquement pour l'user connecté
         if(!$user){
             throw $this->createAccessDeniedException('Vous devez être connecté pour voir votre profil.');
         }
 
+        // on vérifie que le formulaire a été soumis
         if($request->isMethod('POST')){
+            
+            // on récupère les contents des champs (champs textes/fichiers uploadés)
             $pseudo=$request->request->get('pseudo');
             $email=$request->request->get('email');
             $password=$request->request->get('password');
             $profileImage=$request->files->get('profileImage');
             $profileImageName=null;
+
+                // Gestion de l'image de profil
                 if($profileImage){
+
+                    // pour éviter les conflits de noms
                     $profileImageName=uniqid() . '.' . $profileImage->guessExtension();
+                    
+                    // on enregitre l'image
                     $profileImage->move($parameterBag->get(name: 'kernel.project_dir') . '/public/uploads/users', $profileImageName);
+                    // on stocke le nom en base
                     $user->setImage($profileImageName);
                 }
 
-            if(empty($pseudo) || empty($email)){
+                // Si les champs obligatoire sont vide affiche message erreur
+                if(empty($pseudo) || empty($email)){
                 $this->addFlash('error', 'Tous les champs sont requis.');
-
-            }else{
+                
+                // Sinon mise à jour des données
+                }else{
                 $user->setPseudo($pseudo);
                 $user->setEmail($email);
                 $user->setUpdatedAt(new \DateTimeImmutable());
                 
-
+                // optionnelle : mise à jour du mot de passe
                 if(!empty(trim($password))){ //fonction trim => supprime les espaces au début et à la fin de la chaine de caractère $password
                     //hasher de nouveau mot de passe
                     $hashedPassword=$passwordHasher->hashPassword($user, $password);
                     $user->setPassword($hashedPassword);
                 }
 
-                $entityManager->flush();
+                $entityManager->flush(); // on sauvegarde ici pas besoin de persist car l'user existe déjà
 
                 $this->addFlash('success', 'Votre profil mis à jour avec succès.');
                 return $this->redirectToRoute('user-show-profile');
